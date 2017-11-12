a94bbb1f356028261593942a89f1780eb5cb51c0
Karmy Widjaja <karmy0311@gmail.com>
2017-11-12 11:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,4 @@ function destructivelyRemoveFirstDriver(name){
 }
 function appendDriver(name)
 {const addADriver = [...drivers, name]
-  addADriver.push("Broom")
-return addADriver}
-
+return addADriver"Broom"}
